b260de9549ecaba8845ed85351ce67bc9e7857d3
Rinn09 <154898426+Rinn09@users.noreply.github.com>
2024-01-21 13:03:05 +0700

    Create userStats.js

---
+++ userStats.js
@@ -0,0 +1,11 @@
+// Module.exports để sử dụng trong các file khác
+module.exports = {
+  async initialize() {
+  },
+
+  async getUserStats(userId) {
+  },
+
+  async updateUserStats(stats) {
+  },
+};
