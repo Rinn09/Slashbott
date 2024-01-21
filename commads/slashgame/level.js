fd60f94456312048862e1a012dc831d6c7b7968e
Rinn09 <154898426+Rinn09@users.noreply.github.com>
2024-01-21 12:55:13 +0700

    level.js

---
+++ commads/slashgame/level.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const userStats = require('../../userStats');
+const UserStat = require('../../models/UserStat');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('level')
+        .setDescription('Kiểm tra cấp độ, EXP và tiền của bạn.'),
+
+    async execute(interaction) {
+        try {
+            const userId = interaction.user.id;
+            const stats = await userStats.getUserStats(userId);
+
+            const currentLevel = stats.userLevels || 0;
+            const currentEXP = stats.userExp || 0;
+            const currentCash = stats.userCash || 0;
+            const formattedCash = currentCash.toFixed(2);
+
+            await interaction.reply(`Bạn đang ở cấp độ ${currentLevel} với ${currentEXP} EXP và có ${formattedCash}$.`);
+
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'Có lỗi xảy ra khi thực hiện lệnh này!', ephemeral: true });
+        }
+    },
+};
